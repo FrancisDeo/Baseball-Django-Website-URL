3cc582c36bcc9e19a7f3a80484ab54930dc02901
FrancisDeo <103027197+FrancisDeo@users.noreply.github.com>
2023-01-16 12:55:30 +0800

    Delete repository directory

--- repository/projectsite/baseball/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Person, Club, Play, Position
-
-admin.site.register(Person)
-admin.site.register(Club)
-admin.site.register(Play)
-admin.site.register(Position)
